87b56769fdba87c0f9cd2443126cb5f4cc57c380
DragaoTI <140221509+DragaoTI@users.noreply.github.com>
2025-05-11 09:24:45 +0000

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,5 @@
+from .supabase_service import supabase_service # Sua instância existente
+from .admin_service import AdminService
 
+# Use o client da instância supabase_service
+admin_service_instance = AdminService(supabase_client=supabase_service.client)
